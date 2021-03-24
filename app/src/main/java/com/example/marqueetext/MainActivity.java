be953769a2aaa38c500d6cf3d0cd454ed74db3d8
Babitababy <kumaribabita2402@gmail.com>
2021-03-24 17:34:17 +0530

    color changed

--- app/src/main/java/com/example/marqueetext/MainActivity.java
+++ app/src/main/java/com/example/marqueetext/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.marqueetext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtMarquee;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //casting of textview
+        txtMarquee=(TextView)findViewById(R.id.marqueeText);
+        //Now we will call setSelected() method and pass boolean value as true
+        txtMarquee.setSelected(true);
+
 
     }
 }
